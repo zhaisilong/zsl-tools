838b2e65f33786e3e76241edaf651830c5a6419d
zhaisilong <zhaisilong@outlook.com>
2022-03-18 15:07:40 +0800

    fix conf.py

--- source/conf.py
+++ source/conf.py
@@ -23,7 +23,7 @@ author = 'zhaisilong'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1.0'
-
+language = 'zh_CN'
 
 # -- General configuration ---------------------------------------------------
 
